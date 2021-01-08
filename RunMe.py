bd18953df0d84a6b95ae579bfe296a1c123ba07e
David Nadrchal <davidnadrchalintj@gmail.com>
2021-01-08 15:08:43 +0100

    Prepare deck

--- RunMe.py
+++ RunMe.py
@@ -1,1 +1,6 @@
 print("Welcome in Higher Lower Casiono. The higher will be your risk, the lower you'll win!")
+#Prepare deck
+Deck=[]
+for Symbol in ['♠','♡','♢','♣']:
+    for relValue,imageValue in enumerate(['2','3','4','5','6','7','8','9','10','J','Q','K','A']):
+        Deck.append({'image':imageValue+Symbol,'value':relValue})
